a9dec49aae55b8512747cca3a4de2452fde601cd
unknown <sk712851@gmail.com>
2021-12-06 17:45:20 +0700

    first commit

---
+++ app/src/main/java/com/example/prak8_1918051_sandi/webView.java
@@ -0,0 +1,14 @@
+package com.example.prak8_1918051_sandi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class webView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
